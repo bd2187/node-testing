85e7ec65c32ecf02ca6108265011a41e234fc0bc
Brandon Dionisio <brandondionisio@Brandons-MacBook-Air.local>
2017-10-20 10:25:22 -0700

    Initial commit. Set up package.json. Create initial route

---
+++ app.js
@@ -0,0 +1,11 @@
+// 3rd party
+var express = require("express");
+var app = express();
+
+// local
+var routes = require("./controllers");
+routes(app);
+
+// Set up port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Now listening to port: ${PORT}`));
