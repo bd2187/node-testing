5ceac6a807451e31da5ccdfc8f3ba143839343a6
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-10-20 12:56:12 -0700

    Create error route

--- app.test.js
+++ app.test.js
@@ -1,4 +1,7 @@
+// 3rd party
 const request = require("supertest");
+
+// local
 const { app } = require("./app");
 
 it("should display hello world", done => {
--- controllers/index.js
+++ controllers/index.js
@@ -2,4 +2,8 @@ module.exports = function(app) {
   app.get("/", (req, res) => {
     res.send("hello world");
   });
+
+  app.get("/error", (req, res) => {
+    res.status(404).send({ error: "Sorry, page not found" });
+  });
 };
